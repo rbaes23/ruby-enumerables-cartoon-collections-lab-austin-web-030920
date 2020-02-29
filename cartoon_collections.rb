3dc2316df4a1ab5b5ddab3961bce41192d7630cf
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-29 17:44:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,21 @@
 def roll_call_dwarves(dwarves)
-  puts "1. Dopey"
-  puts "2. Grumpy"
-  puts "3. Bashful"
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-# hash = Hash.new
-# %w(cat dog wombat).each_with_index { |item, index|
-#   hash[item] = index
-# }
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
+end
 
-# def summon_captain_planet# code an argument here
-#   # Your code here
-# end
+def long_planeteer_calls(calls_array)
+  calls_array.any? { |call| call.length > 4 }
+end
 
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
-
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
+def find_the_cheese(array_of_strs)
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array_of_strs.find do |food|
+    cheese_types.include? (food)
+  end
+end
--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -17,52 +17,52 @@ describe "Cartoon Collections" do
     end
   end
 
-  # describe "#summon_captain_planet" do
-  #   it "returns an array with the same number of elements that it was given" do
-  #     veggies = ["carrot", "cucumber", "pepper"]
-  #     result = summon_captain_planet(veggies)
-  #     expect(result.class).to eq(Array)
-  #     expect(result.length).to eq(3)
-  #   end
+  describe "#summon_captain_planet" do
+    it "returns an array with the same number of elements that it was given" do
+      veggies = ["carrot", "cucumber", "pepper"]
+      result = summon_captain_planet(veggies)
+      expect(result.class).to eq(Array)
+      expect(result.length).to eq(3)
+    end
 
-  #   it "capitalizes each element and adds an exclamation mark (test 1)" do
-  #     fruits = ["apple", "banana", "orange"]
-  #     result = summon_captain_planet(fruits)
-  #     ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
-  #   end
+    it "capitalizes each element and adds an exclamation mark (test 1)" do
+      fruits = ["apple", "banana", "orange"]
+      result = summon_captain_planet(fruits)
+      ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
+    end
 
-  #   it "capitalizes each element and adds an exclamation mark (test 2)" do
-  #     veggies = ["carrot", "cucumber", "pepper"]
-  #     result = summon_captain_planet(veggies)
-  #     ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
-  #   end
-  # end
+    it "capitalizes each element and adds an exclamation mark (test 2)" do
+      veggies = ["carrot", "cucumber", "pepper"]
+      result = summon_captain_planet(veggies)
+      ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
+    end
+  end
 
-  # describe "#long_planeteer_calls" do
-  #   it "returns true if any calls are longer than 4 characters" do
-  #     calls_long = ["axe", "earth", "wind", "fire"]
-  #     expect(long_planeteer_calls(calls_long)).to eq(true)
-  #   end
+  describe "#long_planeteer_calls" do
+    it "returns true if any calls are longer than 4 characters" do
+      calls_long = ["axe", "earth", "wind", "fire"]
+      expect(long_planeteer_calls(calls_long)).to eq(true)
+    end
 
-  #   it "returns false if all calls are 4 characters or less" do
-  #     calls_short = ["wind", "fire", "tree", "axe", "code"]
-  #     expect(long_planeteer_calls(calls_short)).to eq(false)
-  #   end
+    it "returns false if all calls are 4 characters or less" do
+      calls_short = ["wind", "fire", "tree", "axe", "code"]
+      expect(long_planeteer_calls(calls_short)).to eq(false)
+    end
 
-  # end
+  end
 
-  # describe "#find_the_cheese" do
-  #   it "returns the first element of the array that is cheese" do
-  #     contains_cheddar = ["banana", "cheddar", "sock"]
-  #     expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
+  describe "#find_the_cheese" do
+    it "returns the first element of the array that is cheese" do
+      contains_cheddar = ["banana", "cheddar", "sock"]
+      expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
 
-  #     contains_gouda = ["potato", "gouda", "camembert"]
-  #     expect(find_the_cheese(contains_gouda)).to eq 'gouda'
-  #   end
+      contains_gouda = ["potato", "gouda", "camembert"]
+      expect(find_the_cheese(contains_gouda)).to eq 'gouda'
+    end
 
-  #   it "returns nil if the array does not contain a type of cheese" do
-  #     no_cheese = ["ham", "cellphone", "computer"]
-  #     expect(find_the_cheese(no_cheese)).to eq nil
-  #   end
-  # end
+    it "returns nil if the array does not contain a type of cheese" do
+      no_cheese = ["ham", "cellphone", "computer"]
+      expect(find_the_cheese(no_cheese)).to eq nil
+    end
+  end
 end
