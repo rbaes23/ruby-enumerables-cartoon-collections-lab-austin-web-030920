0ba0c9186aa664674974d813439b82fe35cd2618
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-29 17:22:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  puts "1. Dopey"
+  puts "2. Grumpy"
+  puts "3. Bashful"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+# hash = Hash.new
+# %w(cat dog wombat).each_with_index { |item, index|
+#   hash[item] = index
+# }
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def summon_captain_planet# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
+
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
