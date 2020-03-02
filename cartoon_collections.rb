08f9bd88a90ecba023d59dffee3c9f90f9137457
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-03-02 17:41:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,21 +1,23 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
+  dwarves.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
   end
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capitalize + '!' }
+  planeteer_calls.map do |calls|
+    calls.capitalize + '!'
+  end
 end
 
-def long_planeteer_calls(calls_array)
-  calls_array.any? { |call| call.length > 4 }
+def long_planeteer_calls(calls)
+  calls.any? {|calls| calls.length > 4}
 end
 
-def find_the_cheese(array_of_strs)
+def find_the_cheese(arr_of_strs)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array_of_strs.find do |food|
-    cheese_types.include? (food)
+  arr_of_strs.find do |is_it_cheese|
+    cheese_types.include?(is_it_cheese) 
   end
 end
---
+++ cartoon_collections_again.rb
@@ -0,0 +1,11 @@
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
+end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |calls|
+    puts "#{calls.capitalize} + '!'"
+  end
+end
